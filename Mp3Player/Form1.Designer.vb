db6343d5dd8a4816fdc26cffbeecbf1919ecd289
oltanklo <tankz0r@msn.com>
2015-04-16 12:29:47 -0500

    Updated Mp3 Player

--- Mp3Player/Form1.Designer.vb
+++ Mp3Player/Form1.Designer.vb
@@ -22,34 +22,31 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
-        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.PlayToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.PauseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrevToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NextToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SeekBar = New System.Windows.Forms.ProgressBar()
+        Me.SeekTimer = New System.Windows.Forms.Timer(Me.components)
         Me.MenuStrip1.SuspendLayout()
         CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'MenuStrip1
         '
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem, Me.PlayToolStripMenuItem, Me.PauseToolStripMenuItem, Me.ToolStripMenuItem1, Me.ToolStripMenuItem2})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PlayToolStripMenuItem, Me.PauseToolStripMenuItem, Me.PrevToolStripMenuItem, Me.NextToolStripMenuItem, Me.OpenToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(616, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(302, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
-        '
-        'OpenToolStripMenuItem
-        '
-        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
-        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
-        Me.OpenToolStripMenuItem.Text = "Open"
         '
         'PlayToolStripMenuItem
         '
@@ -63,17 +60,23 @@ Partial Class Form1
         Me.PauseToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
         Me.PauseToolStripMenuItem.Text = "Pause"
         '
-        'ToolStripMenuItem1
+        'PrevToolStripMenuItem
         '
-        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
-        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(35, 20)
-        Me.ToolStripMenuItem1.Text = "<<"
+        Me.PrevToolStripMenuItem.Name = "PrevToolStripMenuItem"
+        Me.PrevToolStripMenuItem.Size = New System.Drawing.Size(35, 20)
+        Me.PrevToolStripMenuItem.Text = "<<"
         '
-        'ToolStripMenuItem2
+        'NextToolStripMenuItem
         '
-        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
-        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(35, 20)
-        Me.ToolStripMenuItem2.Text = ">>"
+        Me.NextToolStripMenuItem.Name = "NextToolStripMenuItem"
+        Me.NextToolStripMenuItem.Size = New System.Drawing.Size(35, 20)
+        Me.NextToolStripMenuItem.Text = ">>"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.OpenToolStripMenuItem.Text = "Open"
         '
         'AxWindowsMediaPlayer1
         '
@@ -87,28 +90,40 @@ Partial Class Form1
         '
         'OpenFileDialog1
         '
-        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.FileName = "SelectFiles"
         Me.OpenFileDialog1.Multiselect = True
         '
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(35, 51)
+        Me.ListBox1.Location = New System.Drawing.Point(12, 49)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(424, 316)
+        Me.ListBox1.Size = New System.Drawing.Size(280, 186)
         Me.ListBox1.TabIndex = 2
+        '
+        'SeekBar
+        '
+        Me.SeekBar.Location = New System.Drawing.Point(12, 33)
+        Me.SeekBar.Name = "SeekBar"
+        Me.SeekBar.Size = New System.Drawing.Size(280, 10)
+        Me.SeekBar.TabIndex = 3
+        '
+        'SeekTimer
+        '
+        Me.SeekTimer.Interval = 1000
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(616, 414)
+        Me.ClientSize = New System.Drawing.Size(302, 244)
+        Me.Controls.Add(Me.SeekBar)
         Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.AxWindowsMediaPlayer1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Mp3 Player"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
         CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -120,10 +135,12 @@ Partial Class Form1
     Friend WithEvents OpenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents PlayToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents PauseToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
-    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
-    Friend WithEvents ToolStripMenuItem2 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrevToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NextToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
     Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
     Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents SeekBar As System.Windows.Forms.ProgressBar
+    Public WithEvents SeekTimer As System.Windows.Forms.Timer
 
 End Class
