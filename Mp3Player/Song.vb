ed87c5038403f3485606ecc021d08f9af8d60291
oltanklo <tankz0r@msn.com>
2015-04-17 11:41:00 -0500

    Updating Mp3

---
+++ Mp3Player/Song.vb
@@ -0,0 +1,26 @@
+﻿Public Class Song
+    Private mp3Title As String
+    Private mp3Dir As String
+
+    Public Property songTitle() As String
+        Get
+            Return Me.mp3Title
+        End Get
+        Set(value As String)
+            Me.mp3Title = value
+        End Set
+    End Property
+
+    Public Property songDir() As String
+        Get
+            Return Me.mp3Dir
+        End Get
+        Set(value As String)
+            Me.mp3Dir = value
+        End Set
+    End Property
+
+    Public Overrides Function ToString() As String
+        Return songTitle
+    End Function
+End Class
