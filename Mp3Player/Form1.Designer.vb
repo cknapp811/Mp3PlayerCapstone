1990aa56d3681abaa2d387f135088457a426dc14
oltanklo <tankz0r@msn.com>
2015-04-24 00:26:07 -0500

    Major updates

--- Mp3Player/Form1.Designer.vb
+++ Mp3Player/Form1.Designer.vb
@@ -26,7 +26,7 @@ Partial Class Form1
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.PlayToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.PauseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StopToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.PrevToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.NextToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
@@ -36,11 +36,17 @@ Partial Class Form1
         Me.SaveListToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
         Me.ClearListToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
         Me.SeekBar = New System.Windows.Forms.ProgressBar()
         Me.SeekTimer = New System.Windows.Forms.Timer(Me.components)
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.ListBox2 = New System.Windows.Forms.ListBox()
         Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.CurrentSeek = New System.Windows.Forms.Label()
+        Me.TotalLength = New System.Windows.Forms.Label()
+        Me.SongTitle = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.MenuStrip1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -49,10 +55,10 @@ Partial Class Form1
         'MenuStrip1
         '
         Me.MenuStrip1.BackColor = System.Drawing.Color.Transparent
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PlayToolStripMenuItem, Me.PauseToolStripMenuItem, Me.PrevToolStripMenuItem, Me.NextToolStripMenuItem, Me.ToolStripMenuItem1, Me.OpenToolStripMenuItem, Me.PlayListToolStripMenuItem})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PlayToolStripMenuItem, Me.StopToolStripMenuItem, Me.PrevToolStripMenuItem, Me.NextToolStripMenuItem, Me.ToolStripMenuItem1, Me.OpenToolStripMenuItem, Me.PlayListToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(462, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(436, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -62,11 +68,11 @@ Partial Class Form1
         Me.PlayToolStripMenuItem.Size = New System.Drawing.Size(41, 20)
         Me.PlayToolStripMenuItem.Text = "Play"
         '
-        'PauseToolStripMenuItem
+        'StopToolStripMenuItem
         '
-        Me.PauseToolStripMenuItem.Name = "PauseToolStripMenuItem"
-        Me.PauseToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
-        Me.PauseToolStripMenuItem.Text = "Pause"
+        Me.StopToolStripMenuItem.Name = "StopToolStripMenuItem"
+        Me.StopToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.StopToolStripMenuItem.Text = "Stop"
         '
         'PrevToolStripMenuItem
         '
@@ -120,14 +126,20 @@ Partial Class Form1
         'OpenFileDialog1
         '
         Me.OpenFileDialog1.FileName = "SelectFiles"
+        Me.OpenFileDialog1.Filter = "MP3 files (*.mp3)|*.mp3|WMA files(*wma)|*wma|All files(*.*)|*.*"
         Me.OpenFileDialog1.Multiselect = True
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.DefaultExt = "txt"
+        Me.OpenFileDialog2.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
         '
         'SeekBar
         '
-        Me.SeekBar.Location = New System.Drawing.Point(0, 27)
+        Me.SeekBar.Location = New System.Drawing.Point(37, 73)
         Me.SeekBar.Name = "SeekBar"
-        Me.SeekBar.Size = New System.Drawing.Size(468, 10)
-        Me.SeekBar.TabIndex = 3
+        Me.SeekBar.Size = New System.Drawing.Size(364, 10)
+        Me.SeekBar.TabIndex = 1
         '
         'SeekTimer
         '
@@ -136,21 +148,23 @@ Partial Class Form1
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(0, 43)
+        Me.PictureBox1.Location = New System.Drawing.Point(533, 73)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(488, 230)
+        Me.PictureBox1.Size = New System.Drawing.Size(411, 242)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 4
         Me.PictureBox1.TabStop = False
         '
         'ListBox2
         '
-        Me.ListBox2.BackColor = System.Drawing.SystemColors.ScrollBar
-        Me.ListBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ListBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.ListBox2.Font = New System.Drawing.Font("Book Antiqua", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ListBox2.FormattingEnabled = True
-        Me.ListBox2.Location = New System.Drawing.Point(23, 59)
+        Me.ListBox2.ItemHeight = 15
+        Me.ListBox2.Location = New System.Drawing.Point(37, 92)
         Me.ListBox2.Name = "ListBox2"
-        Me.ListBox2.Size = New System.Drawing.Size(427, 147)
+        Me.ListBox2.Size = New System.Drawing.Size(364, 122)
         Me.ListBox2.TabIndex = 5
         '
         'AxWindowsMediaPlayer1
@@ -163,12 +177,62 @@ Partial Class Form1
         Me.AxWindowsMediaPlayer1.TabIndex = 1
         Me.AxWindowsMediaPlayer1.Visible = False
         '
+        'CurrentSeek
+        '
+        Me.CurrentSeek.AutoSize = True
+        Me.CurrentSeek.Location = New System.Drawing.Point(4, 73)
+        Me.CurrentSeek.Name = "CurrentSeek"
+        Me.CurrentSeek.Size = New System.Drawing.Size(28, 13)
+        Me.CurrentSeek.TabIndex = 1
+        Me.CurrentSeek.Text = "0:00"
+        '
+        'TotalLength
+        '
+        Me.TotalLength.AutoSize = True
+        Me.TotalLength.Location = New System.Drawing.Point(401, 73)
+        Me.TotalLength.Name = "TotalLength"
+        Me.TotalLength.Size = New System.Drawing.Size(28, 13)
+        Me.TotalLength.TabIndex = 1
+        Me.TotalLength.Text = "0:00"
+        '
+        'SongTitle
+        '
+        Me.SongTitle.Font = New System.Drawing.Font("Matura MT Script Capitals", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SongTitle.Location = New System.Drawing.Point(4, 40)
+        Me.SongTitle.Name = "SongTitle"
+        Me.SongTitle.Size = New System.Drawing.Size(432, 23)
+        Me.SongTitle.TabIndex = 6
+        Me.SongTitle.Text = "Song Title"
+        Me.SongTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GroupBox1.Location = New System.Drawing.Point(-8, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(458, 10)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Location = New System.Drawing.Point(-39, 57)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(490, 10)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(462, 218)
+        Me.ClientSize = New System.Drawing.Size(436, 241)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.SongTitle)
+        Me.Controls.Add(Me.TotalLength)
+        Me.Controls.Add(Me.CurrentSeek)
         Me.Controls.Add(Me.ListBox2)
         Me.Controls.Add(Me.SeekBar)
         Me.Controls.Add(Me.AxWindowsMediaPlayer1)
@@ -188,11 +252,12 @@ Partial Class Form1
     Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
     Friend WithEvents OpenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents PlayToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
-    Friend WithEvents PauseToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents StopToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents PrevToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents NextToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
     Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents OpenFileDialog2 As System.Windows.Forms.OpenFileDialog
     Friend WithEvents SeekBar As System.Windows.Forms.ProgressBar
     Public WithEvents SeekTimer As System.Windows.Forms.Timer
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
@@ -202,5 +267,10 @@ Partial Class Form1
     Friend WithEvents LoadListToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents SaveListToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents ClearListToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CurrentSeek As System.Windows.Forms.Label
+    Friend WithEvents TotalLength As System.Windows.Forms.Label
+    Friend WithEvents SongTitle As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
 
 End Class
--- Mp3Player/Form1.vb
+++ Mp3Player/Form1.vb
@@ -4,55 +4,92 @@ Imports System.IO
 Public Class Form1
     Dim paths As String()
     Dim fileNames As String()
+    Dim song As Song = New Song()
+    Dim SeekSeconds As String
+    Dim PlaySwapper As String = True
 
     Public Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
         If OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
-            OpenFileDialog1.Filter = "MP3 files (*.mp3)|*.mp3|WMA files(*wma)|*wma|All files(*.*)|*.*"
             paths = OpenFileDialog1.SafeFileNames
             fileNames = OpenFileDialog1.FileNames
             For i As Integer = 0 To fileNames.Length - 1
-                Dim song As Song = New Song()
-                song.songTitle = System.IO.Path.GetFileNameWithoutExtension(fileNames(i))
-                song.songDir = fileNames(i)
-                ListBox2.Items.Add(song)
+                song.songTitle = System.IO.Path.GetFileName(fileNames(i))
+                song.songDir = Path.GetDirectoryName(fileNames(i))
+                ListBox2.Items.Add(song.songTitle)
             Next
         End If
     End Sub
 
     Private Sub PlayToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PlayToolStripMenuItem.Click
-        AxWindowsMediaPlayer1.Ctlcontrols.play()
-        AxWindowsMediaPlayer1.URL = ListBox2.SelectedItem
-        SeekBar.Value = 0
-        SeekTimer.Start()
+        Try
+            If PlaySwapper = True Then
+                PlaySwapper = False
+                Me.PlayToolStripMenuItem.Text = "Pause"
+                AxWindowsMediaPlayer1.URL = (song.songDir & "\" & ListBox2.SelectedItem)
+                AxWindowsMediaPlayer1.Ctlcontrols.play()
+                SongTitle.Text = AxWindowsMediaPlayer1.currentMedia.name
+                SeekBar.Value = 0
+                SeekTimer.Start()
+            Else
+                PlaySwapper = True
+                Me.PlayToolStripMenuItem.Text = "Play"
+                AxWindowsMediaPlayer1.Ctlcontrols.pause()
+                SeekTimer.Stop()
+            End If
+        Catch ex As Exception
+            MsgBox("ERROR" & vbCrLf & "PLEASE MAKE SURE A SONG IS SELECTED OR PLAYLIST IS LOADED" & vbCrLf & ex.Message)
+        End Try
     End Sub
 
-    Private Sub PauseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PauseToolStripMenuItem.Click
-        AxWindowsMediaPlayer1.Ctlcontrols.pause()
+    Private Sub StopToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StopToolStripMenuItem.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
         SeekTimer.Stop()
     End Sub
 
     Private Sub PrevToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrevToolStripMenuItem.Click
-        Me.ListBox2.SelectedIndex = Me.ListBox2.SelectedIndex - 1
-        AxWindowsMediaPlayer1.URL = ListBox2.SelectedItem
-        AxWindowsMediaPlayer1.Ctlcontrols.play()
-    End Sub
-
-    Private Sub NextToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NextToolStripMenuItem.Click
-        If ListBox2.Items.Count >= 1 Then
-            Me.ListBox2.SelectedIndex = Me.ListBox2.SelectedIndex + 1
-            AxWindowsMediaPlayer1.URL = ListBox2.SelectedItem
+        If (Me.ListBox2.TopIndex <> ListBox2.SelectedIndex) Then
+            Me.ListBox2.SelectedIndex = Me.ListBox2.SelectedIndex - 1
+            AxWindowsMediaPlayer1.URL = (song.songDir & "\" & ListBox2.SelectedItem)
             AxWindowsMediaPlayer1.Ctlcontrols.play()
+            SongTitle.Text = AxWindowsMediaPlayer1.currentMedia.name
+            SeekBar.Value = 0
+            SeekTimer.Start()
         End If
     End Sub
 
-    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
-        AxWindowsMediaPlayer1.URL = paths(ListBox2.SelectedIndex)
+    Private Sub NextToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NextToolStripMenuItem.Click
+        If ((Me.ListBox2.Items.Count - 1) <> Me.ListBox2.SelectedIndex) Then
+            Me.ListBox2.SelectedIndex = Me.ListBox2.SelectedIndex + 1
+            AxWindowsMediaPlayer1.URL = (song.songDir & "\" & ListBox2.SelectedItem)
+            AxWindowsMediaPlayer1.Ctlcontrols.play()
+            SongTitle.Text = AxWindowsMediaPlayer1.currentMedia.name
+            SeekBar.Value = 0
+            SeekTimer.Start()
+        End If
     End Sub
 
     Private Sub SeekTimer_Tick(ByVal sender As System.Object, ByVal e As EventArgs) Handles SeekTimer.Tick
-        If SeekBar.Value < 100 Then
+        SeekBar.Maximum = AxWindowsMediaPlayer1.currentMedia.duration
+        If (SeekBar.Value < SeekBar.Maximum) Then
             SeekBar.Value = SeekBar.Value + 1
-        Else : SeekBar.Value = 0
+            If SeekSeconds = 59 Then
+                SeekSeconds = "00"
+            End If
+            If SeekSeconds < 9 Then
+                SeekSeconds = ("0" & SeekSeconds + 1)
+            Else : SeekSeconds = SeekSeconds + 1
+            End If
+            CurrentSeek.Text = Math.Floor((AxWindowsMediaPlayer1.currentMedia.duration) / 60) & ":" & SeekSeconds
+            TotalLength.Text = AxWindowsMediaPlayer1.currentMedia.duration
+        Else
+            SeekTimer.Stop()
+            SeekBar.Value = 0
+            If ((Me.ListBox2.Items.Count - 1) <> Me.ListBox2.SelectedIndex) Then
+                Me.ListBox2.SelectedIndex = Me.ListBox2.SelectedIndex + 1
+                AxWindowsMediaPlayer1.URL = (song.songDir & "\" & ListBox2.SelectedItem)
+                AxWindowsMediaPlayer1.Ctlcontrols.play()
+                SeekTimer.Start()
+            End If
         End If
     End Sub
 
@@ -60,28 +97,42 @@ Public Class Form1
         SeekBar.Value = 0
     End Sub
 
-    Private Sub LoadListToolStripMenuItem1_Click_1(sender As Object, e As EventArgs) Handles LoadListToolStripMenuItem1.Click
-        Dim fileReader As System.IO.StreamReader
-        fileReader = My.Computer.FileSystem.OpenTextFileReader("C:\Users\Aaron\Desktop\playlist.txt")
-        Dim stringReader As String
-        Dim HasLine As Boolean = True
-        While HasLine
-            stringReader = fileReader.ReadLine
-            If stringReader Is Nothing Then
-                HasLine = False
-            Else : ListBox2.Items.Add(stringReader)
-            End If
-        End While
-    End Sub
-    Private Sub ClearListToolStripMenuItem1_Click_1(sender As Object, e As EventArgs) Handles ClearListToolStripMenuItem1.Click
-        ListBox2.Items.Clear()
+    Private Sub LoadListToolStripMenuItem1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LoadListToolStripMenuItem1.Click
+        Dim oReader As StreamReader
+        If OpenFileDialog2.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Dim HasLine As Boolean = True
+            oReader = New StreamReader(OpenFileDialog2.FileName, True)
+            While HasLine
+                Dim strReader As String = oReader.ReadLine
+                If strReader Is Nothing Then
+                    HasLine = False
+                Else
+                    song.songTitle = strReader.Split("=")(0)
+                    song.songDir = strReader.Split("=")(1)
+                    ListBox2.Items.Add(song.songTitle)
+                End If
+            End While
+        End If
     End Sub
 
     Private Sub SaveListToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles SaveListToolStripMenuItem1.Click
+        Dim saveAsPlaylist As New SaveFileDialog()
+        saveAsPlaylist.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+        saveAsPlaylist.FilterIndex = 1
+        saveAsPlaylist.RestoreDirectory = True
+        If saveAsPlaylist.ShowDialog() = DialogResult.OK Then
+            Dim FileName As Object = saveAsPlaylist.FileName
+            Using SW As New IO.StreamWriter(FileName, False)
+                For Each objMp3 As String In Me.ListBox2.Items
+                    SW.WriteLine(objMp3 & "=" & song.songDir)
+                Next
+            End Using
+        End If
     End Sub
 
-   
-
+    Private Sub ClearListToolStripMenuItem1_Click_1(sender As Object, e As EventArgs) Handles ClearListToolStripMenuItem1.Click
+        ListBox2.Items.Clear()
+    End Sub
 End Class
 
 
